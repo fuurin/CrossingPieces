ed8f5fbd2df9d7cbc13cc18d908109f3d00b51af
fuurin <s1100617@sendai-nct.jp>
2016-04-20 19:45:24 +0900

    ユーザ情報用DB消しました

--- app/controllers/user_data_controller.rb
+++
@@ -1,2 +0,0 @@
-class UserDataController < ApplicationController
-end
--- test/controllers/user_data_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserDataControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
