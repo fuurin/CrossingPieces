d81e01c3e7a72c370d4fe9624f0d8377c21d5c7b
Minari.Yoshinari <mii.informationsystems@gmail.com>
2016-04-25 00:18:14 +0900

    データベースに大学を追加できるようにしました（画像と地図以外）

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -2,27 +2,37 @@ class UniversitiesController < ApplicationController
 	before_action :authenticate_user!
 	before_action :find_user
 
+
 	def new
 		@university = University.new
 	end
 
 	# def create
 	# 	写真の登録前には、このようにコンテントタイプを保存し、readメソッドを通す必要があります。
- 	# 	if params[:university][:photo1]
+	# 	if params[:university][:photo1]
 	#   	params[:university][:photo1_content_type] = params[:university][:photo1].content_type
 	#     params[:university][:photo1] = params[:university][:photo1].read
 	#   end
- 	# end
+	# end
 
- 	private
+	def create
+		@university = University.new(configure_permitted_parameters)
+		@university.save
+		redirect_to "/home/index"
+	end
 
- 	# DBに保存する前には、このようにパラメータをpermitしなければ保存されません。
- 	# def configure_permitted_parameters
-  # 	permit_params = [:name, :comment, :country_region, :university_id, :photo, :photo_content_type]
-  #   devise_parameter_sanitizer.for(:sign_up) << permit_params
-  # end
+	private
 
-  def find_user
-    @user = User.find(current_user.id)
-  end
+		# DBに保存する前には、このようにパラメータをpermitしなければ保存されません。
+		def configure_permitted_parameters
+			#permit_params = [:name_ja, :name_en,:recommended_point, :comment]
+			#devise_parameter_sanitizer.for(:sign_up) << permit_params
+			logger.debug("ちゃんかわ")
+			logger.debug(params[:university].inspect)
+			params.require(:university).permit(:name_ja, :name_en, :recommended_point, :comment, :created_by, :created_at, :updated_by, :updated_at)
+		end
+
+		def find_user
+			@user = User.find(current_user.id)
+		end
 end
